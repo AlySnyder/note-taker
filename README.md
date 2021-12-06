58e54a1a48fd647900a06c5574e8e115f1c5a93b
Alex Snyder <alystrife@gmail.com>
2021-12-05 19:07:54 -0500

    added updated js file , read me

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# note-taker
-App where you can take real time notes and save to your computer
+# Alex's Note Taker
+![license](https://img.shields.io/badge/license-MIT-orange.svg)
+
+## Description
+
+Note Taker app that allows you to create , save, and delete notes.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [License](#license)
+* [Contributing](#contributing)
+* [Credits](#credits)
+* [Questions](#questions)
+
+## Installation
+Clone my repo to your local machine, and install npm init in your terminal. Then use "index node.js" to run the app.
+
+## Usage
+
+[Walkthrough Video](https://github.com/AlySnyder/Team-Portfolio-Generator/raw/main/team-profile-generator.mov)
+
+## Tests
+
+Tests have not been made for this app. 
+
+## License
+
+This app is governed by MIT license.
+
+## Contributing
+
+For contributing to the improvements of this project, please contact me via email alystrife@gmail.com. You can also find me using my Github handle alysnyder.
+
+## Credits 
+
+Alex Snyder, google.com, stackoverflow 
+
+## Questions
+
+If you have any questions , feel free to reach me via e-mail at alystrife@gmail.com For any code related questions or to look at my other projects , check out my Github at www.github.com/alysnyder
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+const { off } = require('process');
+// const { clog } = require('./middleware/clog');
+
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+// app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './db/db.json'))
+);
+
+app.get('/api/notes/:id', (req, res) => {
+    let dbnotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    res.json(dbnotes[Number(req.params.id)]);
+}
+
+);
+
+// GET Route for all other routes sending index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// route for /api/notes, tbd
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+// post Route for /notes 
+app.post('/api/notes', (req, res) => {
+    let dbnotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    let incomingNote = req.body;
+    let newId = dbnotes.length.toString();
+    incomingNote.id = newId;
+    dbnotes.push(incomingNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbnotes));
+    res.json(dbnotes);
+}
+);
+
+
+// delete Route for /notes/:id
+app.delete('/api/notes/:id', (req, res) => {
+    let dbnotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    let incomingId = req.params.id;
+    let newId = 0;
+    dbnotes = dbnotes.filter((thisNote) => {
+        return thisNote.id !=incomingId;
+    });
+
+    for (thisNote of dbnotes){
+        thisNote.id = newId.toString();
+        newId ++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbnotes));
+    res.json(dbnotes);
+}
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
